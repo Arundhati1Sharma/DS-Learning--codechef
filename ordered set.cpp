46dfcacdd89e691ed51cf4d1cb978c70956ad1bb
Arundhati  Sharma <arundhatisharma1098@gmail.com>
2020-06-22 22:23:20 +0530

    Create ordered set.cpp

---
+++ ordered set.cpp
@@ -0,0 +1,6 @@
+
+// pbds - policy based data structure
+// set - no random access
+// if u wanted to know the kth largest element in a set, u would have to iterate through k elements
+
+// Q queries : insert an element, find the kth largest element in the set
